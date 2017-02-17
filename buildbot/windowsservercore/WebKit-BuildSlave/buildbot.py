ddb95eccf05fa6bfb7c0c5d51045ee179ee1d88a
Don <don.j.olmstead@gmail.com>
2017-02-17 15:50:46 -0800

    Adding buildbot template

---
+++ buildbot/windowsservercore/WebKit-BuildSlave/buildbot.py
@@ -0,0 +1,31 @@
+import os
+
+from template import templated_file
+
+def get_options():
+    return {
+        # buildbot.tac
+        'master': os.environ.get('BUILD_MASTER_NAME', 'build.webkit.org'),
+        'port': os.environ.get('BUILD_MASTER_PORT', '17000'),
+        'slave': os.environ.get('BUILD_SLAVE_NAME'),
+        'password': os.environ.get('BUILD_SLAVE_PASSWORD'),
+        # info/admin
+        'name': os.environ.get('ADMIN_NAME'),
+        'email': os.environ.get('ADMIN_EMAIL'),
+        # info/host
+        'description': os.environ.get('HOST_DESCRIPTION'),
+    }
+
+def process_file(file, options):
+    output = file[:-5]
+
+    templated_file(file, output, options)
+
+def main():
+    options = get_options()
+
+    process_file('buildbot.tac.tmpl', options)
+    process_file('info/admin.tmpl', options)
+    process_file('info/host.tmpl', options)
+
+if __name__ == "__main__": main()
--- buildbot/windowsservercore/WebKit-BuildSlave/template.py
+++ buildbot/windowsservercore/WebKit-BuildSlave/template.py
@@ -1,36 +1,10 @@
-import os
 from string import Template
 
-def get_options():
-    return {
-        # buildbot.tac
-        'master': os.environ.get('BUILD_MASTER_NAME', 'build.webkit.org'),
-        'port': os.environ.get('BUILD_MASTER_PORT', '17000'),
-        'slave': os.environ.get('BUILD_SLAVE_NAME'),
-        'password': os.environ.get('BUILD_SLAVE_PASSWORD'),
-        # info/admin
-        'name': os.environ.get('ADMIN_NAME'),
-        'email': os.environ.get('ADMIN_EMAIL'),
-        # info/host
-        'description': os.environ.get('HOST_DESCRIPTION'),
-    }
+def templated_file(input, output, options):
+    input_file = open(input)
+    output_file = open(output, 'w')
 
-def process_file(file, options):
-    input = open(file)
+    template = Template(input_file.read())
+    result = template.substitute(options)
 
-    src = Template(input.read())
-
-    result = src.substitute(options)
-
-    output = open(file[:-5], 'w')
-
-    output.write(result)
-
-def main():
-    options = get_options()
-
-    process_file('buildbot.tac.tmpl', options)
-    process_file('info/admin.tmpl', options)
-    process_file('info/host.tmpl', options)
-
-if __name__ == "__main__": main()
+    output_file.write(result)
