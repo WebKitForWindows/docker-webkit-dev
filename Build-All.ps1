c5ea70a79a0b72e04a70b11883211d823a952014
Don <don.j.olmstead@gmail.com>
2017-05-15 16:57:19 -0700

    feat(*): Add script to build all images

---
+++ Build-All.ps1
@@ -0,0 +1,22 @@
+Function Build-WebKitDockerImage {
+  Param(
+    [Parameter(Mandatory)]
+    [string] $tag
+  )
+
+  $path = Join-Path $PSScriptRoot (Join-Path $tag 'windowsservercore');
+  $cmd = 'docker build -t webkitdev/{0} {1}' -f $tag, $path;
+
+  Write-Host $cmd;
+  Invoke-Expression $cmd;
+}
+
+# Update Windows image
+Write-Host 'docker pull microsoft/windowsservercore';
+docker pull microsoft/windowsservercore
+
+Build-WebKitDockerImage -Tag scripts;
+Build-WebKitDockerImage -Tag scm;
+Build-WebKitDockerImage -Tag tools;
+Build-WebKitDockerImage -Tag msbuild;
+Build-WebKitDockerImage -Tag buildbot;
