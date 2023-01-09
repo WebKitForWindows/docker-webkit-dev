4c58305310aa60786f3862c7fe10939322a3296c
Stephan Szabo <stephan.szabo@sony.com>
2023-01-09 10:42:04 -0800

    More test

--- Build-All.ps1
+++ Build-All.ps1
@@ -23,7 +23,7 @@ function Build-WebKitDockerImage {
     $buildArgs = ('--build-arg IMAGE_TAG={0}' -f $tag);
   }
 
-  $cmd = 'docker build -t webkitdev/{0}:{1} {2} -f {3} --build-arg HTTPS_PROXY=$env:HTTPS_PROXY --build-arg HTTP_PROXY=$env:HTTP_PROXY -m 2GB {4}' -f $image,$tag,$buildArgs,$file,$path;
+  $cmd = 'docker build -t webkitdev/{0}:{1} {2} -f {3} --build-arg HTTPS_PROXY={5} --build-arg HTTP_PROXY={6} -m 2GB {4}' -f $image,$tag,$buildArgs,$file,$path,$env:HTTPS_PROXY,$env:HTTP_PROXY;
 
   Write-Host ('Starting build at {0}' -f (Get-Date))
   Write-Host $cmd;
